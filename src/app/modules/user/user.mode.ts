4eff58761f15d03f1df0417773db3fc3362c557d
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-12-27 21:02:37 -0800

    added user and blog interface & blog

---
+++ src/app/modules/user/user.mode.ts
@@ -0,0 +1,32 @@
+import { model, Schema } from 'mongoose';
+import { TUser } from './user.interface';
+
+const userSchema = new Schema<TUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      select: false,
+    },
+    isBlocked: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+const User = model<TUser>('User', userSchema);
+
+export default User;
